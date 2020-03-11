7e095544bd81daae6c4e14a151bdf3e43a9a6e42
Luis Gabriel Verola Santos <62057819+LVerola@users.noreply.github.com>
2020-03-11 13:25:16 -0300

    Delete questao06.py

--- questao06.py
+++
@@ -1,6 +0,0 @@
-#Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem. 
-distancia=float(input('Escreva a distancia a ser percorrida em KM: '))
-velocidade=float(input('Escreva a velocidade média esperada para a viagem em KM/h: '))
-tempoHora=distancia/velocidade
-tempoMinutos=tempoHora*60
-print(f'O tempo gasto na viagem é de {tempoMinutos:.2f} Minutos, que equivale a {tempoHora:.2f} Horas.')
