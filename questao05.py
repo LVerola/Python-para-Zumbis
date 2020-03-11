8dda182bd314a8dd73954b3afde5b781e416dacf
Luis Gabriel Verola Santos <62057819+LVerola@users.noreply.github.com>
2020-03-11 13:25:01 -0300

    Delete questao05.py

--- questao05.py
+++
@@ -1,6 +0,0 @@
-#Solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o preço a pagar. 
-Mercadoria=float(input("Digite o preço da mercadoria: "))
-percentualDesconto=float(input("Digite o percentual de desconto: "))
-Desconto=percentualDesconto*Mercadoria/100
-PrecoFinal=Mercadoria-Desconto
-print (f"O desconto foi de R${Desconto:.2f} e o preço a ser pago é de R${PrecoFinal:.2f}")
