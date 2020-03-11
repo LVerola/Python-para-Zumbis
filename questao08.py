907bc71a276d9bce3367dfb947ec27a9d0d682c7
Luis Gabriel Verola Santos <62057819+LVerola@users.noreply.github.com>
2020-03-11 13:27:59 -0300

    Delete questao08.py

--- questao08.py
+++
@@ -1,4 +0,0 @@
-#Faça agora o contrário, de Fahrenheit para Celsius. 
-fahrenheit=float(input('Insira a temperatura em ºF: '))
-conversao=((fahrenheit-32)/1.8)
-print(f'{fahrenheit:.2f}ºF é igual a {conversao:.2f}ºC')
