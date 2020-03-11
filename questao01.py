4bcd21eb04cd3a35c69f9de1ebe6ff5d0a918c53
LVerola <62057819+LVerola@users.noreply.github.com>
2020-03-11 08:24:39 -0300

    Delete questao01.py

--- questao01.py
+++
@@ -1,4 +0,0 @@
-#Faça um programa que peça dois números inteiros e imprima a soma desses dois números 
-n1=int(input("Digite o primeiro número inteiro: "))
-n2=int(input("Digite o segundo número inteiro: "))
-print("A soma de", n1, "com", n2, "é equivalente a: ",n1+n2)
