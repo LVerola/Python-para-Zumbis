9ae06e31ea0487edb4d32e126d4df008efee6c00
Luis Gabriel Verola Santos <62057819+LVerola@users.noreply.github.com>
2020-03-11 13:25:26 -0300

    Delete questao07.py

--- questao07.py
+++
@@ -1,4 +0,0 @@
-#Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32 
-celsius=float(input('Insira a temperatura em ºC: '))
-conversao=1.8*celsius+32
-print(f'{celsius:.2f}ºC é igual a {conversao:.2f}ºF')
