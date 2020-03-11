f6a5327cf7006191a5a2c47530e13cdd12e83fcb
Luis Gabriel Verola Santos <62057819+LVerola@users.noreply.github.com>
2020-03-11 13:28:32 -0300

    Delete questao11.py

--- questao11.py
+++
@@ -1,4 +0,0 @@
-# Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão.
-a=str(2**1000000)
-b=len(a)
-print('A conta 2 elevado a 1.000.000 possui', b,'digitos.')
